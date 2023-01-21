e6fcd5373d4600ffd238cc1ff68e02414f2631c5
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-21 15:05:55 +0530

    initial version

---
+++ src/strignmethods.java
@@ -0,0 +1,47 @@
+public class strignmethods {
+    public static void main(String[] args) {
+
+//        String name = "Mehul";
+//        int value = name.length();
+//        System.out.println(value);
+
+// to convert upper and lower case in any string
+//           String str = "MOhit";
+//           String tolowercase = str.toLowerCase();
+//           String touppercase = str.toUpperCase();
+//           System.out.println(tolowercase);
+//           System.out.println(touppercase);
+
+//    want to print some starting and end stirng in the given string
+//           String name = "Munjpara";
+//           System.out.println(name.substring(1));
+//           System.out.println(name.substring(1,5));
+
+//        want to change string from old to new
+
+//        String str = "Munjpara";
+//        System.out.println(str);
+//        System.out.println(str.replace('a','M'));
+
+//        String name1 = "Swaminarayan";
+//        System.out.println(name1.startsWith("Swa"));
+//        System.out.println(name1.endsWith("min"));
+
+
+// check position in the string
+//        String str = "mohit";
+//        System.out.println(str.charAt(4));
+
+//        String str = "mohit";
+//        System.out.println(str.equals("MEhul"));
+//        System.out.println(str.equals("mohit"));
+
+
+
+
+
+
+
+
+    }
+}
