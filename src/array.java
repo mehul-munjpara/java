01be5b84873ff5da9f02d5a4b6bd9cef51373e63
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-21 12:49:45 +0530

    initial version

---
+++ src/array.java
@@ -0,0 +1,17 @@
+public class array {
+
+
+    public static void main(String[] args){
+        int[] marks = {23,45,78,2225,67};
+        /* marks[0] = 200;
+        marks[ 1] = 10;
+        marks[2] = 60;
+        marks[3] = 30;
+        marks[4] = 80;
+*/
+
+
+        System.out.println(marks[3]);
+
+    }
+}
