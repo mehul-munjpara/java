c9d2743802723597eb249343bf5050b86017a164
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-21 17:34:39 +0530

    initial version

---
+++ src/Conditionals.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Conditionals {
+    public static void main(String[] args) {
+
+//            Scanner sc = new Scanner(System.in);
+//            int age = sc.nextInt();
+
+
+//            if(age > 18){
+//                System.out.println("You are eligible to apply for Licence");
+//            }else{
+//                System.out.println("You are not eligible");
+//            }
+
+//   if , else, ladder
+//        if(age > 18){
+//            System.out.println("You are eligible to apply for Licence");
+//        }else if (age == 18){
+//            System.out.println("You will be eligible to apply for Licence");
+//        }else if (age == 17){
+//            System.out.println("You are eligible to apply for Learning-Licence");
+//        }
+//        else{
+//            System.out.println("You are not eligible");
+//        }
+
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter Your Age:- ");
+//        int age = sc.nextInt();
+//
+//        switch(age){
+//            case 18:
+//                System.out.println("You are adult");
+//                break;
+//            case 30:
+//                System.out.println("You have to marry");
+//                break;
+//            case 60:
+//                System.out.println("You should retired");
+//                break;
+//            default:
+//                System.out.println("Life is a journey");
+//
+//        }
+
+
+    }
+}
