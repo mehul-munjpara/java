a7983eb8a2f50309f2f7e9a3ee20635ec524f3ef
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-24 17:17:49 +0530

    initial version

--- src/patternExample.java
+++ src/patternExample.java
@@ -16,35 +16,62 @@ public class patternExample {
     // input => n=4
     //output => 4 3 2 1
 
-    static void printDesc(int n){
-        for(int i = n; i>=1 ; i--){
-            System.out.print(i+" ");
-        }
-    }
+//    static void printDesc(int n){
+//        for(int i = n; i>=1 ; i--){
+//            System.out.print(i+" ");
+//        }
+//    }
 
     // input=> n = 5
 //    Output = 1 2 3 4 5 4 3 2 1
 
-    static void printasc(int n){
-        for(int i = 1 ; i <= n ; i++){
-            System.out.print(i+" ");
-        }
-    }
-    static void printdes(int n) {
-        for (int i = n; i >= 1; i--) {
-            System.out.print(i + " ");
-        }
-    }
+//    static void printasc(int n){
+//        for(int i = 1 ; i <= n ; i++){
+//            System.out.print(i+" ");
+//        }
+//    }
+//    static void printdes(int n) {
+//        for (int i = n; i >= 1; i--) {
+//            System.out.print(i + " ");
+//        }
+//    }
+
+    // n = 5
+    // Ouptput
+    //1
+    //1 2
+    //1 2 3
+    //1 2 3 4
+    //1 2 3 4 5
+
+      static void starpattern1(int n){
+
+        for(int i = 1 ; i<=n; i++){
+              System.out.print(i+" ");
+          }
+
+      }
+
+
+
+
 
         public static void main (String[] args){
-
 //        printRow(15);
 //        printDesc(10);
-
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
 
-            printasc(n);
-            printdes(n-1);
+//            printasc(n);
+//            printdes(n-1);
+
+            for(int i = 1; i<=n; i++){
+                starpattern1(i);
+                System.out.println();
+            }
         }
     }
+
+
+
+
