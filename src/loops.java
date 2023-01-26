b94647769d48762663af31ce34aea88f487dc333
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-26 10:52:18 +0530

    initial version

--- src/loops.java
+++ src/loops.java
@@ -21,15 +21,31 @@ public class loops {
 //        }while(a<5);
 
 
+// Print Odd Number
+//       Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//
+//        for(int i =1; i<=a; i+=2){
+//            System.out.print(" "+ i);
+//        }
+
+        // Print Even Number
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//
+//        for(int i =0; i<=a; i+=2){
+//            System.out.print(" "+ i);
+//        }
+
+        //decrement the number using loop
 
 //        Scanner sc = new Scanner(System.in);
 //        int a = sc.nextInt();
 //
-//        for(int i =1; i<=a; i++){
-//            System.out.println(i);
+//        for(int i =a; i>=0; i--){
+//            System.out.print(" "+ i);
 //        }
 
-        
 
 
 
