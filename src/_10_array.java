793f157f8c1431688681b797f04ee50e93f55ff0
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-27 17:48:19 +0530

    initial version

--- src/_10_array.java
+++ src/_10_array.java
@@ -2,14 +2,36 @@ public class _10_array {
 
 
     public static void main(String[] args){
+
+      //1st method
         int[] marks = {23,45,78,2225,67};
-        /* marks[0] = 200;
-        marks[1] = 10;
-        marks[2] = 60;
-        marks[3] = 30;
-        marks[4] = 80;
-*/
-        System.out.println(marks[3]);
+
+        System.out.println(marks[4]);
+
+
+
+     //2nd method
+
+        int[] marks2;
+        marks2 = new int[5];
+        marks2[0] = 200;
+        marks2[1] = 10;
+        marks2[2] = 60;
+        marks2[3] = 30;
+        marks2[4] = 80;
+        System.out.println(marks2[3]);
+
+
+        //3rd method
+
+        int[] marks3 = new int[5];
+        marks3[0] = 200;
+        marks3[1] = 10;
+        marks3[2] = 60;
+        marks3[3] = 30;
+        marks3[4] = 80;
+        System.out.println(marks3[1]);
+
 
     }
 }
