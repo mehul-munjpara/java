074bb841596f7a65bd9a1bb0ee7cf313bd9d9d79
mehul munjpara <mehulmunjpara13@gmail.com>
2023-01-31 18:33:31 +0530

    initial version

--- src/_10_array.java
+++ src/_10_array.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class _10_array {
 
 
@@ -5,41 +7,54 @@ public class _10_array {
 
         //There Are Three Methods for array
       //1st method
-        int[] marks = {23,45,78,2225,67};
-        System.out.println(marks[4]);
+//        int[] marks = {23,45,78,2225,67};
+//        System.out.println(marks[4]);
 
      //2nd method
-
-        int[] marks3 = new int[5];
-        marks3[0] = 200;
-        marks3[1] = 10;
-        marks3[2] = 60;
-        marks3[3] = 30;
-        marks3[4] = 80;
-        System.out.println(marks3[1]);
+//
+//        int[] marks3 = new int[5];
+//        marks3[0] = 200;
+//        marks3[1] = 10;
+//        marks3[2] = 60;
+//        marks3[3] = 30;
+//        marks3[4] = 80;
+//        System.out.println(marks3[1]);
 
 
         //3rd method
-        int[] marks2;
-        marks2 = new int[5];
-        marks2[0] = 200;
-        marks2[1] = 10;
-        marks2[2] = 60;
-        marks2[3] = 30;
-        marks2[4] = 80;
-        System.out.println(marks2[3]);
+//        int[] marks2;
+//        marks2 = new int[5];
+//        marks2[0] = 200;
+//        marks2[1] = 10;
+//        marks2[2] = 60;
+//        marks2[3] = 30;
+//        marks2[4] = 80;
+//        System.out.println(marks2[3]);
 
 
         // Print All Array's using for loop
+//        double[] sub = new double[4];
+//        sub[0] = 4242.2323;
+//        sub[1] = 454.003;
+//        sub[2] = 111.5545;
+//        sub[3] = 888.76;
+//
+//        for (int i = 0; i<4; i++ ){
+//            System.out.println(sub[i]);
+//        }
 
-        double[] sub = new double[4];
-        sub[0] = 4242.2323;
-        sub[1] = 454.003;
-        sub[2] = 111.5545;
-        sub[3] = 888.76;
+        //Take Array From User
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
 
-        for (int i = 0; i<4; i++ ){
-            System.out.println(sub[i]);
+        int number[] = new int[size];
+//Input
+        for (int i = 0 ; i<size; i++){
+            number[i] = sc.nextInt();
+        }
+//Output
+        for(int i = 0; i<size; i++){
+            System.out.print(" " +number[i]);
         }
 
     }
