ee86cbdf932ecc21e5c03f3789b274dece8096f0
mehul munjpara <mehulmunjpara13@gmail.com>
2023-02-03 16:09:08 +0530

    initial version

--- src/_14_String.java
+++ src/_14_String.java
@@ -4,24 +4,23 @@ public class _14_String {
 
     public static void main(String[] args) {
 //        String Way1 = "Mehul";
-//
+
 //        Scanner sc = new Scanner(System.in);
 //        String str1 = sc.nextLine();
+
 ////Find Which Char is in the which position
 //        System.out.println(str1.charAt(2));
-//
+
 ////Converrt string to character Array
 //        char[] arr = str1.toCharArray();
 
         //combine two string
-
         String firstname = "Mehul";
         String lastname = "Munjpara";
         String Fullname = firstname + " "+lastname;
         System.out.println(Fullname);
 
         //For length
-
         System.out.println(Fullname.length());
 
 
