62ab70eab138b0840414d9200a9a8c808401c299
mehul munjpara <mehulmunjpara13@gmail.com>
2023-02-11 11:29:59 +0530

    initial version

--- src/_16_BitMsnipulation.java
+++ src/_16_BitMsnipulation.java
@@ -1,13 +1,25 @@
 public class _16_BitMsnipulation {
     public static void main(String[] args) {
         int n = 5;
-        int pos = 3;
+        int pos = 2;
         int bitMask = 1<<pos;
 
-        if ((bitMask & n) == 0){
-            System.out.println("Bit Was Zero");
-        }else {
-            System.out.println("Bit Was One");
-        }
+        //Get [AND( & ) ] Bit
+//        if ((bitMask & n) == 0){
+//            System.out.println("Bit Was Zero");
+//        }else {
+//            System.out.println("Bit Was One");
+//        }
+
+        //set [OR ( | ) ] Bit
+//       int newNumber = bitMask | n;
+//        System.out.println(newNumber);
+
+
+        //Clear Bit [ AND with NOT (~) ]
+        int notBitMask = ~(bitMask);
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
+
     }
 }
