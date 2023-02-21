08a0537d09bd232c920d905112b0edca1ffa9343
mehul munjpara <mehulmunjpara13@gmail.com>
2023-02-21 12:34:28 +0530

    initial version

---
+++ src/BasicToAdvance/BasicToAdvance.java
@@ -0,0 +1,21 @@
+package BasicToAdvance;
+
+public class BasicToAdvance {
+
+    public static void main(String[] args){
+
+       /* String name ="Mehul";
+        System.out.println(name);
+
+        int a = 45,x=56,y=67;
+        double b= 45.22;
+        boolean isAdult = false;
+        System.out.println(x);
+        System.out.println(b);
+        System.out.println(isAdult);
+*/
+           String str = "Mohit";
+           String str2 = "Munjpara";
+           System.out.println(str + "_" +str2);
+    }
+}
