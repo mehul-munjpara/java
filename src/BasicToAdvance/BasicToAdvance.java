19cab5aaa574a7c62bf678c8339dd4012ce30801
Mehul <mehulmunjpara13@gmail.com>
2023-03-01 21:55:58 +0530

    initial version

--- src/BasicToAdvance/BasicToAdvance.java
+++ src/BasicToAdvance/BasicToAdvance.java
@@ -82,6 +82,14 @@ public class BasicToAdvance {
 //               }
 //           }
 
+//        System.out.print("Enter Numbers:- ");
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//
+//        for (int i = 1; i<=10; i++){
+//            System.out.println(num*i);
+//        }
+
 //  Arrays
 //
 //        int[] marks = {1,3,5,7,8};
